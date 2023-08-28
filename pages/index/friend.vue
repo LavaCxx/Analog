a7ae643be2e1467737817ad59ee8924bed23055d
lavac <lavac@foxmail.com>
2023-08-28 17:08:43 +0800

    fix:try to fix polyfill

--- pages/index/friend.vue
+++ pages/index/friend.vue
@@ -1,6 +1,6 @@
 <template lang='pug'>
 .friend-page.h-full.grid.grid-cols-3.gap-x-6.gap-y-8.auto-rows-min.box-border.overflow-y-auto.pb-10
-    //- FriendCard(v-for="friend in data" :key="friend._id" v-bind="friend")
+    FriendCard(v-for="friend in data" :key="friend._id" v-bind="friend")
 </template>
 
 <script lang="ts" setup>
