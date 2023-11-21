dfacfe96e77840feaf3765a2d6d26cbf131632a4
lavac <lavac@foxmail.com>
2023-11-21 13:53:33 +0800

    feat:add clarity

--- components/widgets/Weather.vue
+++ components/widgets/Weather.vue
@@ -1,42 +1,43 @@
 <template lang="pug">
 .flex.items-center.gap-x-1.text-main
-  <!-- .text-6xl(v-for="icon in icons" :key="icon" v-show="icon===weatherData.icon" :class="icon") -->
   .text-4xl(:class="weatherData.icon")
   .flex.flex-col
     .text-xl {{weatherData.temp}}°C
     .text-xl {{weatherData.location}} {{weatherData.weather}}
 </template>
 <script lang="ts" setup>
-const weatherData=ref({
-  temp:0,
-  weather:'',
-  location:'',
-  code:'',
-  icon:''
-})
-const weatherIcons={
-  'qing':'i-mdi-white-balance-sunny',
-  'yin':'i-mdi-weather-partly-cloudy',
-  'lei':'i-mdi-weather-lightning',
-  'shachen':'i-mdi-weather-dust',
-  'wu':'i-mdi-weather-fog',
-  'bingbao':'i-mdi-weather-hail',
-  'yun':'i-mdi-weather-cloudy',
-  'yu':'i-mdi-weather-rainy',
-}
-const getWeatherData=async()=> {
-  const {week,city,wea,wea_img,tem}=await $fetch('https://v0.yiketianqi.com/api?appid=84429454&appsecret=1wtaJnSw&version=v61', {
-    method: 'GET'
-  })
-  weatherData.value={
-    temp:tem,
-    location:city,
-    icon:weatherIcons[wea_img],
-    weather:wea
-  }
-
-}
-onMounted(()=>{
-  getWeatherData()
-})
+const weatherData = ref({
+  temp: 0,
+  weather: "",
+  location: "",
+  code: "",
+  icon: "",
+});
+const weatherIcons = {
+  qing: "i-mdi-white-balance-sunny",
+  yin: "i-mdi-weather-partly-cloudy",
+  lei: "i-mdi-weather-lightning",
+  shachen: "i-mdi-weather-dust",
+  wu: "i-mdi-weather-fog",
+  bingbao: "i-mdi-weather-hail",
+  yun: "i-mdi-weather-cloudy",
+  yu: "i-mdi-weather-rainy",
+};
+const getWeatherData = async () => {
+  const { week, city, wea, wea_img, tem } = await $fetch(
+    "https://v0.yiketianqi.com/api?appid=84429454&appsecret=1wtaJnSw&version=v61",
+    {
+      method: "GET",
+    },
+  );
+  weatherData.value = {
+    temp: tem,
+    location: city,
+    icon: weatherIcons[wea_img],
+    weather: wea,
+  };
+};
+onMounted(() => {
+  getWeatherData();
+});
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,3 @@
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 // import Unocss from 'unocss/vite'
 // import prismjs from 'vite-plugin-prismjs'
@@ -6,24 +5,24 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
-      WALINE_SERVER_URL: process.env.WALINE_SERVER_URL
-    }
+      WALINE_SERVER_URL: process.env.WALINE_SERVER_URL,
+    },
   },
-  modules: [
-    '@unocss/nuxt',
-    '@nuxt/content',
-    'dayjs-nuxt'
-  ],
+  modules: ["@unocss/nuxt", "@nuxt/content", "dayjs-nuxt"],
   content: {
     highlight: {
       theme: {
-        default: 'github-light',
-        dark: 'github-dark',
-        sepia: 'monokai'
-      }
-    }
+        default: "github-light",
+        dark: "github-dark",
+        sepia: "monokai",
+      },
+    },
   },
-  css: ['@unocss/reset/tailwind.css', '@waline/client/dist/waline.css', '~/assets/styles/main.scss'],
+  css: [
+    "@unocss/reset/tailwind.css",
+    "@waline/client/dist/waline.css",
+    "~/assets/styles/main.scss",
+  ],
   vite: {
     // plugins: [
     //   prismjs({
@@ -37,26 +36,32 @@ export default defineNuxtConfig({
     // ]
   },
   app: {
-    pageTransition: { name: 'page', mode: 'out-in' },
+    pageTransition: { name: "page", mode: "out-in" },
     head: {
-      title: 'LavaCx',
-      link: [
-        { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' }
-      ],
+      title: "LavaCx",
+      link: [{ rel: "icon", type: "image/x-icon", href: "favicon.ico" }],
       script: [
         {
-          src: 'https://umami.lavac.cc/script.js',
+          src: "https://umami.lavac.cc/script.js",
           async: true,
           defer: true,
-          'data-website-id': '80dbf1fa-b47e-429d-9846-5684097fc7a9'
-        }
-      ]
-    }
+          "data-website-id": "80dbf1fa-b47e-429d-9846-5684097fc7a9",
+        },
+        {
+          type: "text/javacscript",
+          innerHTML: `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "jttpq59f1l");`,
+        },
+      ],
+    },
   },
   components: [
     {
-      path: '~/components/',
+      path: "~/components/",
       pathPrefix: false,
     },
   ],
-})
+});
